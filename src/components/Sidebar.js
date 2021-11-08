7f09db98772562b0dcfcc22988d50edf1978b707
María Molina García <86959823+mariamolinagarcia@users.noreply.github.com>
2021-11-08 10:56:33 +0100

    basic mobile version

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,35 +1,50 @@
-
+import burger from '../images/menu-icon.png';
+import '../styles/sidebar.scss';
+import { useState } from 'react';
 const Sidebar = ()=>{
 
+  const [burgerMenu, setBurgerMenu ] = useState('hidden');
+
+  const showBurgerMenu =()=>{
+    if(burgerMenu === 'hidden'){
+      setBurgerMenu('');
+
+    }else{
+      setBurgerMenu('hidden');
+    }
+  }
 
     return (
-        
-    <nav>
-       <ul>
-         Sobre mí
-         <li>
-         </li>
-         <li>
-         Experiencia
-         </li>
-         <li>
-         Formación
-         </li>
-         <li>
-         Aptitudes
-         </li>
-         <li>
-         Proyectos
-         </li>
-         <li>
-         Sobre mí
-         </li>
-         <li>
-         Contacto
-         </li>
-       </ul>
-     </nav>
-
+    <div className='sidebar'>
+      <div className='sidebar__burger' onClick={showBurgerMenu}>
+        <img className='sidebar__burger--img' src={burger} alt='menu burger' title='menu burger' />
+      </div>
+      <nav className={`sidebar__nav ${burgerMenu}`}>
+        <ul>
+          Sobre mí
+          <li>
+          </li>
+          <li>
+          Experiencia
+          </li>
+          <li>
+          Formación
+          </li>
+          <li>
+          Aptitudes
+          </li>
+          <li>
+          Proyectos
+          </li>
+          <li>
+          Sobre mí
+          </li>
+          <li>
+          Contacto
+          </li>
+        </ul>
+      </nav>
+  </div>  
         
     )
 }
