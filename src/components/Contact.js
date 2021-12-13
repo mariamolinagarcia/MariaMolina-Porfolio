8f8e253d498bdc5fdf8b2c1e3d9d1912aa3058cf
María Molina García <86959823+mariamolinagarcia@users.noreply.github.com>
2021-12-13 13:11:55 +0100

    contact css and responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,36 @@ const Contact = ()=>{
     return (
         <div id='contact'>
             <h3><i class="fas fa-address-card"></i> Contacto</h3>
+            <div className='contact__div'>
+            <form className='contact__form'
+            action="https://formspree.io/f/xpzbzlrw"
+            method="POST">
+                <label>
+                    Tu nombre:
+                <input type="text" name="name" />
+                </label>
+                <label>
+                    Tu email:
+                <input type="email" name="_replyto" required />
+                </label>
+                <label>
+                Tu mensaje:
+                <textarea name="message" />
+                </label>
+  
+                 <button type="submit">Enviar</button>
+            </form>
+             
+            <ul className='contact__div--icons'>
+                <li><h5>Otras formas de contacto:</h5></li>
+                <li><a href='mailto:mariquillamolina@gmail.com' alt='mariquillamolina@gmail.com' target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i></a></li>
+                <li><a href='https://github.com/mariamolinagarcia' alt='github' target="_blank" rel="noopener noreferrer"><i class="fab fa-github-alt"></i></a></li>
+                <li><a href='https://twitter.com/MomoHorror' alt='MomoHorror' target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a></li>
+
+            </ul>
+
+            
+            </div>
 
         </div>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
-
+import '../styles/footer.scss';
 const Footer = ()=>{
 
 
     return (
-        <div>
-
+        <div className='footer'>
+            <p>Este portfolio está realizado en ReactJS, utilizando components y hooks.</p>
+            <p>Proyecto responsive desarrollado con Mobile First.</p>
+            <p>Maquetación con SASS y metodología BEM.</p>
         </div>
     )
 }
