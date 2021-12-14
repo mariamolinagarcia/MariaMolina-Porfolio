f500f49b59d47016d90545f2262e44f6f179c701
María Molina García <86959823+mariamolinagarcia@users.noreply.github.com>
2021-12-14 14:24:11 +0100

    projects css transition

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,3 @@
-import rickAndMorty from '../images/RickandMorty.png';
-import reactMigration from '../images/Migracion-React-servidor-express.png';
-import buscadorSeries from '../images/JS-Vanila-Buscador_de_series-detalle.png';
-import landingAwesome from '../images/Landing-Awesome_profile-cards.png';
-import evaluacionMaquetacion from '../images/Módulo_1_Evaluación_final.png'
 
 
 import '../styles/projects.scss';
@@ -20,12 +15,43 @@ const Projects = ()=>{
            <div className='projects__container'>
             <ul className='projects__grid'>
                 <li className='projects__grid--item1'>
-                <img className='projects__grid--item1__photo' src={rickAndMorty} alt='' title='' />
+                <div className='projects__grid--text'>
+                    <h4>Rick y Morty </h4>
+                    <p>Proyecto hecho con React, utilizando components, hooks y API.</p>
+                    <button><a href='https://mariamolinagarcia.github.io/modulo-3-evaluacion-final-mariamolinagarcia/#/' alt='' title='' target="_blank" rel="noopener noreferrer">Ver página</a></button>
+                    <button><a href='https://github.com/mariamolinagarcia/modulo-3-evaluacion-final-mariamolinagarcia' alt='' title='' target="_blank" rel="noopener noreferrer">Ver proyecto</a></button>
+
+                </div>
+                
                 </li>
-                <li className='projects__grid--item2'><img className='projects__grid--item2__photo' src={reactMigration} alt='' title='' /></li>
-                <li className='projects__grid--item3'><img className='projects__grid--item3__photo' src={buscadorSeries} alt='' title='' /></li>
-                <li className='projects__grid--item4'><img className='projects__grid--item4__photo' src={landingAwesome} alt='' title='' /></li>
-                <li className='projects__grid--item5'><img className='projects__grid--item5__photo' src={evaluacionMaquetacion} alt='' title='' /></li>
+                <li className='projects__grid--item2'><div className='projects__grid--text'>
+                    <h4>Awesome Profile </h4>
+                    <p>Proyecto en equipo, de JS a React, código heredado. Servidor con Express y SQLite.</p>
+                    <button><a href='https://react-machines.herokuapp.com/#/' alt='' title='' target="_blank" rel="noopener noreferrer">Ver página</a></button>
+                    <button><a href='https://github.com/mariamolinagarcia/project-promo-n-module-3-team-3' alt='' title='' target="_blank" rel="noopener noreferrer">Ver proyecto</a></button>
+
+                </div></li>
+                <li className='projects__grid--item3'><div className='projects__grid--text'>
+                    <h4>TVShows List </h4>
+                    <p>Proyecto en JS Vanila. Llamada a una API, búsqueda y favoritos en Local Storage. </p>
+                    <button><a href='https://mariamolinagarcia.github.io/modulo-2-evaluacion-final-mariamolinagarcia/' alt='' title='' target="_blank" rel="noopener noreferrer">Ver página</a></button>
+                    <button><a href='https://github.com/mariamolinagarcia/modulo-2-evaluacion-final-mariamolinagarcia' alt='' title='' target="_blank" rel="noopener noreferrer">Ver proyecto</a></button>
+
+                </div></li>
+                <li className='projects__grid--item4'><div className='projects__grid--text'>
+                    <h4>Awesome Profile en JS </h4>
+                    <p>Proyecto hecho en equipo con JS Vanila. Generador de tarjetas de visita. Maquetación con CSS y SASS.</p>
+                    <button><a href='https://mariamolinagarcia.github.io/project-promo-n-module-2-team-1/' alt='' title='' target="_blank" rel="noopener noreferrer">Ver página</a></button>
+                    <button><a href='https://github.com/mariamolinagarcia/project-promo-n-module-2-team-1' alt='' title='' target="_blank" rel="noopener noreferrer">Ver proyecto</a></button>
+
+                </div></li>
+                <li className='projects__grid--item5'><div className='projects__grid--text'>
+                    <h4>Landing</h4>
+                    <p>Proyecto hecho con HTML, CSS y SASS. Resposive, Mobile First.</p>
+                    <button><a href='https://mariamolinagarcia.github.io/modulo-1-evaluacion-final-mariamolinagarcia/' alt='' title='' target="_blank" rel="noopener noreferrer">Ver página</a></button>
+                    <button><a href='https://github.com/mariamolinagarcia/modulo-1-evaluacion-final-mariamolinagarcia' alt='' title='' target="_blank" rel="noopener noreferrer">Ver proyecto</a></button>
+
+                </div></li>
             </ul>
             </div>
         </div>
